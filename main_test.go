00ba206eab6e0ada464ebf0f0d2868954404239d
kamsandhu93 <kamsandhu93@users.noreply.github.com>
2022-01-03 16:35:10 +0000

    [SBG-4] Adding core logic for the roulette game, with support for straights only for now, working tests simulating a staight loss and win

--- main_test.go
+++ main_test.go
@@ -22,9 +22,11 @@ func TestHealthRoute(t *testing.T) {
 }
 
 // Critical Path tests
-func TestRouletteStraight(t *testing.T) {
-	mockWinningNumber := 32
-	expectedWinnings := 100
+func TestRouletteStraightWin(t *testing.T) {
+	inputBetType := "17"
+	inputBetSize := 2
+	mockWinningNumber := 17
+	expectedWinnings := 72
 
 	router := setupRouter(func() int {
 		return mockWinningNumber
@@ -32,8 +34,51 @@ func TestRouletteStraight(t *testing.T) {
 
 	bet := roulette.Bet{
 		ID:   "1",
-		Size: 1,
-		Type: "17",
+		Size: inputBetSize,
+		Type: inputBetType,
+	}
+
+	reqBody, err := json.Marshal(roulette.RequestPayload{
+		UserID:        "1",
+		CorrelationId: "1",
+		Bets:          []roulette.Bet{bet},
+	})
+
+	if err != nil {
+		t.Fatal("[ERROR] Unable to generate the input json for the test", err)
+	}
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/v1/roulette", bytes.NewBuffer(reqBody))
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+
+	var respBody roulette.ResponsePayload
+	if err := json.Unmarshal(w.Body.Bytes(), &respBody); err != nil {
+		t.Fatal("[ERROR] Unable to deserialize JSON response", err)
+	}
+
+	assert.Equal(t, expectedWinnings, respBody.Winnings)
+	assert.Equal(t, mockWinningNumber, respBody.WinningNumber)
+
+}
+
+// Critical Path tests
+func TestRouletteStraightLoss(t *testing.T) {
+	inputBetType := "17"
+	inputBetSize := 2
+	mockWinningNumber := 13
+	expectedWinnings := 0
+
+	router := setupRouter(func() int {
+		return mockWinningNumber
+	})
+
+	bet := roulette.Bet{
+		ID:   "1",
+		Size: inputBetSize,
+		Type: inputBetType,
 	}
 
 	reqBody, err := json.Marshal(roulette.RequestPayload{
