572904284812f5c79b2e1e770efb1e3f97e0a8f7
kamsandhu93 <kamsandhu93@users.noreply.github.com>
2022-01-03 17:35:16 +0000

    [SBG-8] Middleware for custom logging and auth

---
+++ middleware/logging.go
@@ -0,0 +1,20 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"time"
+)
+
+func Logger() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		t := time.Now()
+
+		c.Next()
+
+		latency := time.Since(t)
+		status := c.Writer.Status()
+
+		log.Printf("Latency=%d Status=%d", latency, status)
+	}
+}
